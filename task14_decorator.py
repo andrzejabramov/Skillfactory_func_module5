7cfdac8209892d44d948fc6bbf71f82f15b9ba09
andrejabramov <andrzejvod@gmail.com>
2025-03-01 22:49:57 +0300

    Skillfactory_func_module5_10

--- task14_decorator.py
+++ task14_decorator.py
@@ -1,2 +1,21 @@
-def retry_if_result_is_none():
-    pass
+import random
+
+
+def retry_if_result_is_none(times=1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                res = func(*args, **kwargs)
+                if res != None:
+                    return res
+                return None
+        return wrapper
+    return decorator
+
+
+@retry_if_result_is_none(times=2)
+def test_function():
+    return random.choice([None, 'Passed'])
+
+
+print(test_function())
